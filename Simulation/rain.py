98d5ba05b7f6db556035f34c3ab3d7328b7c2ef3
dotping-me <137102636+dotping-me@users.noreply.github.com>
2024-07-11 22:52:22 +0400

    Added more functionality
    Added a region of force around cursor (toggled by a left click) which pushed droplets away to simulate an umbrella. The speeds at which the droplets fall also vary at random times.

--- Simulation/rain.py
+++ Simulation/rain.py
@@ -1,4 +1,14 @@
+from math import exp
+
 import pygame, random
+
+def calculate_length_between_2_coords(coords_1 : tuple[int], coords_2 : tuple[int]) -> int :
+	
+	# Pythagoras theorum
+	dx_square : int = pow(abs(coords_1[0] - coords_2[0]), 2)
+	dy_square : int = pow(abs(coords_1[1] - coords_2[1]), 2)
+
+	return int(pow((dx_square + dy_square), 0.5))
 
 class Droplet :
 	
@@ -11,8 +21,13 @@ class Droplet :
 
 	ACCEL_DUE_TO_GRAV : float = 9.81
 
-	def __init__(self, x_max : int, foreground_level : float = 1.0) -> None :
+	def __init__(self, x_max : int, foreground_level : float = 1.0, cursor_influence_radius : int = 0) -> None :
 		
+		self.foreground_level = foreground_level
+
+		# To simulate the effect of an umbrella
+		self.cursor_influence_radius : int = cursor_influence_radius
+
 		# The smallest mass of a droplet can be estimated to be 4e-8 Kg
 		# The greatest mass of a droplet can be estimated to be 0.0003 Kg (= 30 000e-8 Kg)
 		
@@ -50,7 +65,7 @@ class Droplet :
 		self.y : float = - 0.525007 / self.mass
 
 		# Droplets on lower planes will fall slower
-		self.accel : float = foreground_level * self.ACCEL_DUE_TO_GRAV
+		self.accel : float = self.foreground_level * self.ACCEL_DUE_TO_GRAV
 
 		# For rendering,
 		# The greater the mass of the droplet, the more pixels it takes
@@ -72,6 +87,36 @@ class Droplet :
 
 		# Accelerates droplets
 		self.y += self.accel
+
+		# Push droplets to the side
+		if self.cursor_influence_radius :
+
+			# Gets position of the mouse
+			mouse_x, mouse_y = pygame.mouse.get_pos()
+
+			# Within range
+			dist_from_cursor : int = calculate_length_between_2_coords(coords_1 = (self.x, self.y), coords_2 = (mouse_x, mouse_y))
+			
+			# The region of force around the cursor is a semi-ellipse
+			if (dist_from_cursor <= self.cursor_influence_radius) and (self.y >= mouse_y - (self.cursor_influence_radius // 2)) :
+
+				# Moves droplet according to y = 1 / ln x curve
+				
+				# Where x : Force to push droplet
+				#       y : Distance from cursor
+
+				force : int = int(exp(6 / (dist_from_cursor + 1)) + self.accel)
+
+				# Moves to the right
+				if self.x >= mouse_x :
+					pass
+
+				# Moves to the left
+				else :
+					force = -1 * force
+
+				# Moves droplet
+				self.x += force
 
 	def draw(self) -> None :
 
@@ -95,12 +140,12 @@ class Droplet :
 			# Else, rainfall would follow a static pattern which is not natural
 			self.y : float = (- 0.525007 / self.mass) * (random.randint(5, 10) / 10)
 
-def get_rain(x_max : int, n_droplets : int = 100, n_foreground_levels : int = 1) -> list[Droplet] :
+def get_rain(x_max : int, n_droplets : int = 100, n_foreground_levels : int = 1, cursor_influence_radius : int = 0) -> list[Droplet] :
 
 		# Creates Droplet objects
 		return [
 			
-			Droplet(x_max = x_max, foreground_level  = 1 - (fg_level / 10))
+			Droplet(x_max = x_max, foreground_level  = 1 - (fg_level / 10), cursor_influence_radius = cursor_influence_radius)
 				for _ in range(n_droplets // n_foreground_levels)
 					for fg_level in range(n_foreground_levels)
 
@@ -121,22 +166,35 @@ if __name__ == '__main__' :
 	clock  : pygame.time.Clock = pygame.time.Clock()
 
 	# Hides cursor
-	pygame.mouse.set_visible(False)
+	# pygame.mouse.set_visible(False)
+
+	# Default values
+	max_n_droplets          : int = 1000
+	n_foreground_levels     : int = 2
+	cursor_influence_radius : int = int(0.1 * screen_x) 
 
 	# Setup particles
-	rain : list[Droplet]    = get_rain(
-		x_max               = screen_x,
-		n_droplets          = 1000,
-		n_foreground_levels = 2
+	rain : list[Droplet] = get_rain(
+		x_max                   = screen_x,
+		n_droplets              = max_n_droplets,
+		n_foreground_levels     = n_foreground_levels,
+		cursor_influence_radius = 0
 		)
 
 	# Pygame loop
-	simulating : bool  = True
+	event_toggle : bool = False
+	simulating   : bool = True
+	umbrella     : bool = False
+	fps          : int  = 120
+
+	# Custom event (Called each x frames)
+	heavy_rain : int = pygame.USEREVENT + 1
+	pygame.time.set_timer(heavy_rain, int(fps * 100))
 
 	while simulating :
 
 		# Limits while loop iteration speed in FPS
-		clock.tick(120)
+		clock.tick(fps)
 
 		for event in pygame.event.get() :
 
@@ -146,6 +204,53 @@ if __name__ == '__main__' :
 				
 				# Ends sim
 				simulating = False
+
+			# Custom event
+			if event.type == heavy_rain :
+				
+				# Varies rain
+				if (event_toggle == False) and (random.choice([True, False])) :
+					event_toggle = True
+
+					# 1 : Varies acceleration
+
+					# Generates a random float from 0.75 to 1.25
+					accel_variation : float = random.randint(75, 125) / 100
+
+					# Updates acceleration of droplets
+					for i in rain :
+						i.accel = accel_variation * i.accel
+
+				# Resets droplets
+				else :
+					event_toggle = False
+
+					# Resets acceleration of droplets
+					for i in rain :
+						i.accel = i.foreground_level * Droplet.ACCEL_DUE_TO_GRAV
+
+			# Toggle umbrella effect
+			if event.type == pygame.MOUSEBUTTONDOWN :
+				
+				# Activates umbrella effect
+				if not umbrella :
+					umbrella = True
+
+				# Deactivates umbrella effect
+				else :
+					umbrella = False
+
+		# Activates umbrella effect gradually
+		if (umbrella) and (rain[0].cursor_influence_radius < cursor_influence_radius) :
+
+			for i in rain :
+				i.cursor_influence_radius += 1
+
+		# Deactives umbrella effect gradually
+		elif (not umbrella) and (rain[0].cursor_influence_radius > 0) :
+			
+			for i in rain :
+				i.cursor_influence_radius -= 1
 
 		if simulating :
 			
